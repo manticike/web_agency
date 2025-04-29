6363dda15dd83885eabd820dab2273b1dbb1bc3b
Isaac Antwi <antwikisaac@gmail.com>
2025-04-29 16:38:56 +0000

    Made changes to the next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'flowbite.s3.amazonaws.com',
-        pathname: '/docs/gallery/masonry/**', // double asterisks mean "any file under that folder"
+        // pathname: '/docs/gallery/masonry/**', // double asterisks mean "any file under that folder"
       },
     ],
   },
